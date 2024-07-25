cc77ad118f231815b46c57f0bb435497b9f0d51c
FrancieleCSantos <francielecoceicaodossantos93@gmail.com>
2024-07-25 19:27:49 -0300

    Realizado a criação de classes

---
+++ src/accounts/account.model.ts
@@ -0,0 +1,9 @@
+export class Account {
+    constructor(
+      public id: number,
+      public name: string,
+      public balance: number
+    ) {}
+  }
+
+  
---
+++ src/custumer/custumer.model.ts
@@ -0,0 +1,11 @@
+export class Custumer {
+    constructor(
+      public id: number,
+      public name: string,
+      public birth: Date,
+      public age: number,
+      public CPF: number
+    ) {}
+  }
+
+  
